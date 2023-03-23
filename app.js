e0a5db9adc7271cb7ac9ae60cae3a510a3bd01e8
LukeNukeEm <110535038+LWolfaardt@users.noreply.github.com>
2023-03-23 19:02:35 +1030

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(
 );
 // DATABASE
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  port: process.env.DB_PORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME,
 });
 
 
